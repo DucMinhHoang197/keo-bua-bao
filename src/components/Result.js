579cb65b112c389c865a2f60e240155d8471622f
DucMinhHoang197 <ducminhhoang197@gmail.com>
2024-01-22 14:26:14 +0700

    fix result

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Result({ user1GameItem, user2GameItem }) {
+function Result({ user1GameItem, user2GameItem, result }) {
   return (
     <div>
       <h1 className="result-header">Result</h1>
       <div className="result-wrapper">
         {user1GameItem && <span>{user1GameItem.name}</span>}
-        <span className="result">{Result}</span>
+        <span className="result">{result}</span>
         {user2GameItem && <span>{user2GameItem.name}</span>}
       </div>
     </div>
